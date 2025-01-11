f0df64ba35e9ac91c609aa8697473e3cf0ba2e1c
thisaramax20 <thisarathejana20@gmail.com>
2025-01-11 15:45:22 +0530

    initialize mobile navbar design

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -11,12 +11,24 @@ const MobileMenu = () => {
         className="flex flex-col cursor-pointer gap-1"
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
-        <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
-        <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        ></div>
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "opacity-0" : ""
+          } ease-in-out duration-500`}
+        ></div>
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "-rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        ></div>
       </div>
       {isOpen && (
-        <div>
+        <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
           <Link href={""}>Home</Link>
           <Link href={""}>Friends</Link>
           <Link href={""}>Groups</Link>
