84350d30ee23eeb7c9a0fe08e444b5241d2f5d70
thisaramax20 <thisarathejana20@gmail.com>
2025-01-22 19:48:50 +0530

    override scroll bar default behavior

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -3,11 +3,111 @@ import React from "react";
 
 const Stories = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm">
-      <div className="flex gap-8">
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* story */}
         <div className="flex flex-col gap-2 items-center cursor-pointer">
           <Image
-            src={""}
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt={""}
+            width={80}
+            height={80}
+            className="rounded-full ring-2 ring-pink-500 w-20 h-20"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/29625547/pexels-photo-29625547/free-photo-of-nighttime-ferris-wheel-with-reflections.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
             alt={""}
             width={80}
             height={80}
